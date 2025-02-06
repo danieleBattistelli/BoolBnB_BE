d16c966a5e1e7e5cc31597e89839ef9b3a5c4519
danieleBattistelli <bomber81@gmail.com>
2025-02-06 16:58:37 +0100

    fix node modules

---
+++ BE/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import immobiliRouter from "./routers/immobili.js";
+import recensioniRouter from "./routers/recensioni.js";
+import notFoundPage from './middlewares/notFoundRoute.js';
+import handleError from "./middlewares/handleError.js";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
+})
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+}))
+
+// impostazioni 
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// rotte
+
+app.use("/immobili", immobiliRouter);
+
+app.use("/recensioni", recensioniRouter);
+
+
+// middlewares di errore
+app.use(notFoundPage.notFoundRoute);
+
+app.use(handleError);
