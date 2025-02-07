e9af0107699069e7ee1f13d5412e77da8ae6b5de
AlessandroSottosanti <asottosanti2@gmail.com>
2025-02-07 11:05:30 +0100

    feat: api INDEX

--- controllers/immobileController.js
+++ controllers/immobileController.js
@@ -3,16 +3,30 @@ import connection from "../data/db.js";
 
 const index = (req, res, next) => {
 
-    const sql = `SELECT immobili.*, AVG(recensioni.voto) AS avg_voto
+    const sql = `SELECT immobili.*,  CAST(AVG(recensioni.voto) AS FLOAT) AS voto_medio
     FROM immobili
     INNER JOIN recensioni
     ON recensioni.id_immobile = immobili.id
-    GROUP BY immobili.id AND avg_voto.
+    GROUP BY recensioni.id
     `;
-    return res.status(200).json({
-        status: "success",
-        message: "Sono una lista di immobili!",
-    });
+
+    connection.query(sql, (err, results) => {
+        if(err) {
+            return res.status(500).json({ status: "error", message: err});
+        }
+        else if (results.length === 0) {
+            return res.status(404).json({
+                status:"error",
+                message: "nessun immobile trovato",
+            });
+        }
+
+        return res.status(200).json({
+            status:"success",
+            results: results,
+        })
+    })
+    
 };
 
 const show = (req, res, next) => {
